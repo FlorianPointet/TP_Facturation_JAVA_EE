16351d1314019de4772a8e655ca520f9ce77d9b3
FlorianPointet <45174397+FlorianPointet@users.noreply.github.com>
2021-02-03 22:05:28 +0100

    Update HomepageServlet.java

--- src/main/java/fr/epsi/controller/HomepageServlet.java
+++ src/main/java/fr/epsi/controller/HomepageServlet.java
@@ -6,6 +6,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/*
+	J'ai décidé de créer une page par fonction (client, article, facture, lignefacture, formulaires...).
+	Petit problème, je n'ai pas réussi à connecter mon projet à ma base de données MySQL. Je n'ai pas réussi à corriger l'erreur "Failed to connect". 
+	J'ai vérifié plusieurs fois mes identifiants, ainsi que l'url de ma base de données. Sans succès. 
+	J'ai donc toutes les pages avec les connexions entre-elles mais sans la base de données...
+
+*/
 public class HomepageServlet extends HttpServlet {
 	
 	@Override
